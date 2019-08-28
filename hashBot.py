bb47ea67016a49a927dbead669e4fdaed6e75d49
Daniel Kuzmin <danielkuzmin@gmail.com>
2019-08-28 12:16:32 -0700

    Input validation

--- hashBot.py
+++ hashBot.py
@@ -1,11 +1,13 @@
 import discord                  # Discord.py API
 import requests                 # To download the files
 import os                       # To create the necessary directories
+import hashlib                  # To hash the given file
+
 from datetime import datetime   # To print system date and time
 from credentials import TOKEN   # Bot's token contained in credentials.py (ignored by git)
 
 # Version Number
-VERSION = "v0.5 - BETA BUILD"
+VERSION = "v0.5 - ALPHA BUILD"
 
 # A list of commands with brief descriptions
 help_block = "```" \
@@ -15,8 +17,10 @@ help_block = "```" \
             "\n$about - Sends a message containing information about this bot to the channel it was invoked in." \
             "```"
 
-# A list of supported hashes
-supported_hashes = "```" \
+supported_hashes = ['SHA1', 'SHA256']
+
+# A list of supported hashes as a string (to print)
+supported_hashes_s = "```" \
                    "---List of Supported Hashes---\n" \
                    "MD2, MD4, MD5, SHA1, SHA256" \
                    "```"
@@ -35,11 +39,21 @@ def create_directories():
         pass
 
 
+# Returns the hash of a file given the filename and hash function name
+#def hash_file(filename, hashname):
+
+
+
 # Saves the attached file to the temp folder
 def save_to_temp(url, a_id, filename):
     r = requests.get(url)
     with open(f"HASHBOT_FILES\TEMPFILES\{a_id}-{filename}", 'wb') as f:
         f.write(r.content)
+
+
+# Checks if the user entered a valid hash
+def validate_input(hashname):
+    return hashname in supported_hashes
 
 
 # Message printed to the console when the bot starts
@@ -71,7 +85,7 @@ async def on_message(message):
         if message.author == client.user:
             return
         await message.channel.send(about_string)
-        await message.channel.send(supported_hashes)
+        await message.channel.send(supported_hashes_s)
 
     # $hash command
     if '$hash' in message.content:
@@ -84,8 +98,26 @@ async def on_message(message):
             await message.channel.send(help_block)
             return
 
-        # Everything below occurs only if a file is attached
+        # Catches the user not inputting a hash name
+        try:
+            hashname = message.content.split()[1]
+        except IndexError:
+            await message.channel.send("```Error: No hash name after $hash command call.```")
+            await message.channel.send(help_block)
+            return
+
+        # Checks if the hash name is valid
+        if not validate_input(hashname):
+            await message.channel.send("```Error: Unsupported hash entered or incorrect hash name.```")
+            await message.channel.send(supported_hashes_s)
+            return
+
+        # Everything below occurs only if a file is attached and the hash name is valid
         print(message.attachments[0])
+        # Saves the file to temp directory
         save_to_temp(message.attachments[0].url, message.attachments[0].id, message.attachments[0].filename)
+
+        # Hashing the file
+        #hash_file(f"{message.attachments[0].id}-{message.attachments[0].filename}", hashname)
 
 client.run(TOKEN)
